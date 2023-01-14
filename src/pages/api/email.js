47e3f3d4e381a8a3620210e1116148d66ca118ab
Restyled.io <commits@restyled.io>
2023-01-14 05:20:09 +0000

    Restyled by clang-format

--- src/pages/api/email.js
+++ src/pages/api/email.js
@@ -2,19 +2,19 @@ const mail = require('@sendgrid/mail');
 
 mail.setApiKey(process.env.SENDGRID_API_KEY)
 
-async function emailBusinessOwner(req, res ) {
-    /* 
-        Expects a `name` and `mobileNumber` in the request body as a JSON object.
-        Also expects a storeOwner ID - but currently is hardcoded.
-    */
-    // req body contains business owner store number
+async function emailBusinessOwner(req, res) {
+  /*
+      Expects a `name` and `mobileNumber` in the request body as a JSON object.
+      Also expects a storeOwner ID - but currently is hardcoded.
+  */
+  // req body contains business owner store number
 
-    const body = JSON.parse(req.body);
-    const storeOwnerEmail = "ashrielbrian@yahoo.com" // hardcoded for now
+  const body = JSON.parse(req.body);
+  const storeOwnerEmail = "ashrielbrian@yahoo.com" // hardcoded for now
 
-    console.log("Potential customer: ", body);
+  console.log("Potential customer: ", body);
 
-    const message = `
+  const message = `
         👋 A potential customer has come knocking on your door!
         <br>
         🧍 Name: <strong>${body.name}</strong>
@@ -36,7 +36,7 @@ async function emailBusinessOwner(req, res ) {
         console.log("SENT!")
         return res.status(200).json({status: 'OK'})
     }).catch(err => {
-        console.log(err);
+    console.log(err);
     })
 }
 
