78a9025cbebfb59b7e1befe0221f169698f61c51
Restyled.io <commits@restyled.io>
2023-01-14 03:36:47 +0000

    Restyled by prettier

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,21 @@
 import "@/styles/globals.css";
 
-import {ChakraProvider} from "@chakra-ui/react";
-import {createBrowserSupabaseClient} from "@supabase/auth-helpers-nextjs";
-import {Session, SessionContextProvider} from "@supabase/auth-helpers-react";
-import {useState} from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { Session, SessionContextProvider } from "@supabase/auth-helpers-react";
+import { useState } from "react";
 
-export default function App({Component, pageProps}) {
-
+export default function App({ Component, pageProps }) {
   const [supabaseClient] = useState(() => createBrowserSupabaseClient());
 
   return (
     <ChakraProvider>
       <SessionContextProvider
-  supabaseClient = {supabaseClient} initialSession =
-      {pageProps.initialSession} >
-
-      <Component { ...pageProps } />
-      </SessionContextProvider><
-      /ChakraProvider>
+        supabaseClient={supabaseClient}
+        initialSession={pageProps.initialSession}
+      >
+        <Component {...pageProps} />
+      </SessionContextProvider>
+    </ChakraProvider>
   );
 }
