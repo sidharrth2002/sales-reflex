96d1943de18e8157db341533ff6ce4b5386d3044
Sidharrth Nagappan <sidharrth2002@gmail.com>
2023-01-14 07:35:37 +0100

    image upload

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,5 +1,4 @@
 import {
-  Box,
   Button,
   Card,
   CardBody,
@@ -7,7 +6,6 @@ import {
   CardHeader,
   Flex,
   FormControl,
-  FormHelperText,
   FormLabel,
   Heading,
   Icon,
@@ -20,8 +18,8 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
-  Select,
   SimpleGrid,
+  Skeleton,
   Text,
   Textarea,
   VStack,
@@ -29,75 +27,84 @@ import {
   useToast,
 } from "@chakra-ui/react";
 import { FaPencilAlt, FaPlus, FaTrash } from "react-icons/fa";
+import { useEffect, useState } from "react";
 
+import { FileUploader } from "react-drag-drop-files";
 import Head from "next/head";
 import { Inter } from "@next/font/google";
-import { useState, useEffect } from "react";
-
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Register() {
-  const supabase = useSupabaseClient();
-
-  useEffect(() => {
-
-    const getProducts = async () => {
-      const {data, error} = await supabase.from("product")
-        .select("*")
-      console.log("supaProducts", data);
-    }
-
-    getProducts();
-  }, [])
-
-  const [products, setProducts] = useState([
-    {
-      name: "Sambal",
-      price: 10,
-      description:
-        "Sambal is a spicy condiment made from a paste of ground chilies, shallots, garlic, and other seasonings. It is a staple condiment in Indonesian, Malaysian, Singaporean, and Bruneian cuisine.",
-      image:
-        "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/e8547f4b-df54-4d29-a432-629ce45f8aaa/Derivates/312a8b0d-199e-4b0c-b4cf-8ce9509ea6b0.jpg",
-    },
-    {
-      name: "Kuey Teow",
-      price: 10,
-      description:
-        "Kuey teow is a flat rice noodle dish that is popular in Southeast Asia. It is a staple food in Malaysia, Singapore, Indonesia, Brunei, and southern Thailand. It is also popular in the southern Chinese provinces of Guangdong, Fujian, and Hainan.",
-      image:
-        "https://rasamalaysia.com/wp-content/uploads/2009/11/char-koay-teow-thumb.jpg",
-    },
-    {
-      name: "Asam Laksa",
-      price: 10,
-      description:
-        "Asam laksa is a spicy noodle soup dish that is popular in Malaysia, Singapore, and Indonesia. It is a staple food in the Peranakan cuisine of Southeast Asia.",
-      image:
-        "https://www.rotinrice.com/wp-content/uploads/2014/09/AsamLaksa-1.jpg",
-    },
-  ]);
+  const [products, setProducts] = useState([]);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [productName, setProductName] = useState("");
   const [productPrice, setProductPrice] = useState(0);
   const [productDescription, setProductDescription] = useState("");
   const [productImage, setProductImage] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
+  const [loading, setLoading] = useState(false);
+  const supabase = useSupabaseClient();
+
+  useEffect(() => {
+    const getProducts = async () => {
+      setLoading(true);
+      const { data, error } = await supabase.from("product").select("*");
+      if (error) {
+        console.log("error", error);
+      } else {
+        setProducts(data);
+      }
+      setLoading(false);
+    };
+
+    getProducts();
+  }, []);
 
   const createProduct = (name, price, description, image) => {
     const newProduct = {
       name,
       price,
       description,
-      image,
+      image_path: image,
     };
+    // add to supabase
+    supabase
+      .from("product")
+      .insert(newProduct)
+      .then((response) => {
+        console.log(response);
+      });
+
     setProducts([...products, newProduct]);
   };
 
   const deleteProduct = (name) => {
-    const newProducts = products.filter((product) => product.name !== name);
-    setProducts(newProducts);
+    // delete from supabase
+    supabase
+      .from("product")
+      .delete()
+      .match({ name })
+      .then((response) => {
+        console.log(response);
+        const newProducts = products.filter((product) => product.name !== name);
+        setProducts(newProducts);
+      });
+  };
+
+  const uploadImage = async (file) => {
+    // upload image to supabase
+    const { data, error } = await supabase.storage
+      .from("product-images")
+      .upload(file.name, file);
+    if (error) {
+      console.log("error", error);
+    } else {
+      setProductImage(
+        `https://malkpiqslwdctbpgjzzw.supabase.co/storage/v1/object/public/product-images/${data.path}`
+      );
+    }
   };
 
   const toast = useToast();
@@ -110,141 +117,143 @@ export default function Register() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Flex
-        justifyContent={"center"}
-        alignItems={"center"}
-        width={1000}
-        maxWidth={"80%"}
-        margin={"auto"}
-        padding={12}
-        flexDirection={"column"}
-        boxShadow={"2xl"}
-        mt={20}
-      >
-        <Heading className={inter.className} mb={7}>
-          Manage your products
-        </Heading>
-
+      <Skeleton isLoaded={!loading}>
         <Flex
-          width={300}
-          justifyContent={"space-between"}
+          justifyContent={"center"}
           alignItems={"center"}
-          mb={5}
+          width={1000}
+          maxWidth={"80%"}
+          margin={"auto"}
+          padding={12}
+          flexDirection={"column"}
+          boxShadow={"2xl"}
+          mt={20}
         >
-          <FormControl id="search" mb={5}>
-            <Input
-              type="text"
-              placeholder="Search"
-              onChange={(e) => setSearchTerm(e.target.value)}
-            />
-          </FormControl>
-        </Flex>
+          <Heading className={inter.className} mb={7}>
+            Manage your products
+          </Heading>
 
-        <SimpleGrid
-          spacing={4}
-          templateColumns="repeat(auto-fill, minmax(220px, 1fr))"
-          justifyItems={"center"}
-        >
-          {products
-            .filter((product) => {
-              if (searchTerm === "") {
-                return product;
-              } else if (
-                product.name.toLowerCase().includes(searchTerm.toLowerCase())
-              ) {
-                return product;
-              }
-            })
-            .map((product) => (
-              <Card key={product.name} width={"100%"}>
-                <CardHeader
-                  display={"flex"}
-                  justifyContent={"space-between"}
-                  flexDirection={"row"}
-                  mb={-4}
-                >
-                  <Heading size="md">{product.name}</Heading>
-                  <Text color={"blue.600"}>RM {product.price}</Text>
-                </CardHeader>
-                <CardBody>
-                  <Text mb={8}>
-                    {product.description.substring(0, 100) + "..."}
-                  </Text>
-                  <Flex
-                    alignItems={"center"}
-                    width={"100%"}
-                    justifyContent={"center"}
+          <Flex
+            width={300}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+            mb={5}
+          >
+            <FormControl id="search" mb={5}>
+              <Input
+                type="text"
+                placeholder="Search"
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+            </FormControl>
+          </Flex>
+
+          <SimpleGrid
+            spacing={4}
+            templateColumns="repeat(auto-fill, minmax(220px, 1fr))"
+            justifyItems={"center"}
+          >
+            {products
+              .filter((product) => {
+                if (searchTerm === "") {
+                  return product;
+                } else if (
+                  product.name.toLowerCase().includes(searchTerm.toLowerCase())
+                ) {
+                  return product;
+                }
+              })
+              .map((product) => (
+                <Card key={product.name} width={"100%"}>
+                  <CardHeader
+                    display={"flex"}
+                    justifyContent={"space-between"}
+                    flexDirection={"row"}
+                    mb={-4}
                   >
-                    <Image
-                      borderRadius={10}
-                      src={product.image}
-                      width={250}
-                      maxWidth={"90%"}
-                      alt="product"
+                    <Heading size="md">{product.name}</Heading>
+                    <Text color={"blue.600"}>RM {product.price}</Text>
+                  </CardHeader>
+                  <CardBody>
+                    <Text mb={8}>
+                      {product.description.substring(0, 100) + "..."}
+                    </Text>
+                    <Flex
+                      alignItems={"center"}
+                      width={"100%"}
+                      justifyContent={"center"}
+                    >
+                      <Image
+                        borderRadius={10}
+                        src={product.image_path}
+                        width={250}
+                        maxWidth={"90%"}
+                        alt="product"
+                        textAlign={"center"}
+                      />
+                    </Flex>
+                  </CardBody>
+                  <CardFooter display={"flex"} justifyContent={"space-between"}>
+                    <Button
                       textAlign={"center"}
-                    />
-                  </Flex>
-                </CardBody>
-                <CardFooter display={"flex"} justifyContent={"space-between"}>
-                  <Button
-                    textAlign={"center"}
-                    onClick={() => {
-                      deleteProduct(product.name);
-                      toast({
-                        title: "Product deleted.",
-                        description: "We've deleted the product for you.",
-                        status: "success",
-                        duration: 9000,
-                        isClosable: true,
-                        position: "top",
-                      });
-                    }}
-                  >
-                    <Icon as={FaTrash} color={"red.800"} />
-                  </Button>
-                  <Button textAlign={"center"}>
-                    <Icon as={FaPencilAlt} />
-                  </Button>
-                </CardFooter>
-              </Card>
-            ))}
-          <Card width={"100%"}>
-            <CardHeader
-              display={"flex"}
-              justifyContent={"space-between"}
-              flexDirection={"row"}
-              mb={-4}
-            ></CardHeader>
-            <CardBody>
-              <Flex
-                alignItems={"center"}
-                width={"100%"}
-                justifyContent={"center"}
-              >
-                <VStack
-                  backgroundColor={"gray.100"}
-                  borderRadius={10}
-                  p={8}
-                  display={"flex"}
-                  flexDirection={"column"}
+                      onClick={() => {
+                        deleteProduct(product.name);
+                        toast({
+                          title: "Product deleted.",
+                          description: "We've deleted the product for you.",
+                          status: "success",
+                          duration: 9000,
+                          isClosable: true,
+                          position: "top",
+                        });
+                      }}
+                    >
+                      <Icon as={FaTrash} color={"red.800"} />
+                    </Button>
+                    <Button textAlign={"center"}>
+                      <Icon as={FaPencilAlt} />
+                    </Button>
+                  </CardFooter>
+                </Card>
+              ))}
+            <Card width={"100%"}>
+              <CardHeader
+                display={"flex"}
+                justifyContent={"space-between"}
+                flexDirection={"row"}
+                mb={-4}
+              ></CardHeader>
+              <CardBody>
+                <Flex
                   alignItems={"center"}
+                  width={"100%"}
                   justifyContent={"center"}
-                  onClick={onOpen}
-                  spacing={4}
                 >
-                  <Icon
-                    color={"#0f0f0f"}
-                    as={FaPlus}
-                    fontSize={"5xl"}
+                  <VStack
+                    backgroundColor={"gray.100"}
+                    borderRadius={10}
+                    p={8}
+                    display={"flex"}
+                    flexDirection={"column"}
+                    alignItems={"center"}
+                    justifyContent={"center"}
                     onClick={onOpen}
-                  />
-                  <Text>Add a new product</Text>
-                </VStack>
-              </Flex>
-            </CardBody>
-          </Card>
-        </SimpleGrid>
-      </Flex>
+                    spacing={4}
+                  >
+                    <Icon
+                      color={"#0f0f0f"}
+                      as={FaPlus}
+                      fontSize={"5xl"}
+                      onClick={onOpen}
+                    />
+                    <Text>Add a new product</Text>
+                  </VStack>
+                </Flex>
+              </CardBody>
+            </Card>
+          </SimpleGrid>
+        </Flex>
+      </Skeleton>
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
@@ -274,6 +283,17 @@ export default function Register() {
                   onChange={(e) => setProductPrice(e.target.value)}
                 />
               </FormControl>{" "}
+              <FormControl>
+                <FormLabel>Image</FormLabel>
+                <FileUploader
+                  handleChange={(e) => {
+                    console.log(e);
+                    uploadImage(e);
+                  }}
+                  name="file"
+                  types={["JPG", "JPEG", "PNG", "GIF"]}
+                />
+              </FormControl>
             </VStack>
           </ModalBody>
           <ModalFooter
