3debd24511e473c432c5e7c395f9d4f377e3841f
ziqinyeow <ziqinyeow@gmail.com>
2023-01-27 23:47:54 +0800

    Update

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,8 +4,6 @@ import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import Layout from "components/Layout";
 import { useEffect, useState } from "react";
-import Store from "components/Store";
-import Register from "components/Register";
 
 const inter = Inter({ subsets: ["latin"] });
 
